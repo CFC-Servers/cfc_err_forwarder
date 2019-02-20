5da52f9e57fffd4cec91d6a2ba7018abe7784882
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-02-20 04:48:06 -0500

    First working version!

--- lua/autorun/server/sv_err_fwd.lua
+++ lua/autorun/server/sv_err_fwd.lua
@@ -10,29 +10,31 @@ local forwardingAddress = "http://localhost:5000/webhooks/gmod/forward-errors"
 
 function CFCErrorForwarder.receiveLuaError( isRunTime, fullError, sourceFile, sourceLine, errorStr, stack )
     print( "[CFC Error Forwarder] Received lua error!" )
-    if CFCErrorForwarder.errorQueue[tostring(errorStr)] then
-        --local count = errQueue[errorStr]["count"]
-        --CFCErrorForwarder.errorQueue[errorStr]["count"] = count + 1
-        --CFCErrorForwarder.errorQueue[errorStr]["occuredAt"] = tostring(os.time())
+    if CFCErrorForwarder.errorQueue[errorStr] then
+        local count = CFCErrorForwarder.errorQueue[errorStr]["count"]
+
+        CFCErrorForwarder.errorQueue[errorStr]["count"] = count + 1
+        CFCErrorForwarder.errorQueue[errorStr]["occuredAt"] = os.time()
 
         return
     end
 
     local struct = {}
 
-    struct["isRunTime"] = tostring(isRunTime)
-    --struct["fullError"] = tostring(fullError)
-    --struct["sourceFile"] = tostring(sourceFile)
-    --struct["sourceLine"] = tostring(sourceLine)
-    --struct["errorStr"] = tostring(errorstr)
-    --struct["stack"] = tostring(stack)
-    --struct["occuredAt"] = tostring(os.time())
-    --struct["count"] = 1
+    struct["isRunTime"] = isRunTime
+    struct["fullError"] = fullError
+    struct["sourceFile"] = sourceFile
+    struct["sourceLine"] = sourceLine
+    struct["errorStr"] = errorStr
+    struct["stack"] = stack
+    struct["occuredAt"] = os.time()
+    struct["count"] = 1
 
     print( "[CFC Error Forwarder] Inserting lua error into queue.." )
 
+    CFCErrorForwarder.errorQueue[errorStr] = struct
 
-    CFCErrorForwarder.errorQueue["test"] = struct
+    PrintTable(CFCErrorForwarder.errorQueue)
 end
 
 local function onSuccess( result )
@@ -48,14 +50,11 @@ end
 
 function CFCErrorForwarder.forwardError( obj )
     print("[CFC Error Forwarder] Forwaring lua error(s)!")
-    http.Post( forwardingAddress, obj, onSuccess, onFailure )
-end
+    local json = util.TableToJSON( obj )
+    local data = {}
+    data.json = json
 
-local function combineErrors( queuedErrors )
-    local struct = {}
-    struct.errors = queuedErrors
-
-    return struct
+    http.Post( forwardingAddress, data, onSuccess, onFailure )
 end
 
 function CFCErrorForwarder.groomQueue()
@@ -67,13 +66,11 @@ function CFCErrorForwarder.groomQueue()
     --print( "[CFC Error Forwarder] Error Queue length: " .. tostring( errCount ))
     if errCount == 0 then return end
 
-    print( "[CFC Error Forwarder] Error Queue is not empty (#" .. tostring( errCount ) .. "). Combining errors.." )
+    print( "[CFC Error Forwarder] Error Queue is not empty (#" .. tostring( errCount ) .. ")" )
 
-    local combinedErrors = combineErrors( errQueue )
-
-    print( "[CFC Error Forwarder] Errors combined. Forwarding.." )
-
-    CFCErrorForwarder.forwardError( combinedErrors )
+    for err, data in pairs(errQueue) do
+        CFCErrorForwarder.forwardError( data )
+    end
 
     CFCErrorForwarder.errorQueue = {}
 end
