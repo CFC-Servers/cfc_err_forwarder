8e0584ba91a37cef4b78f8e56c96a334d46e2f34
Redox <git@redox.to>
2025-04-13 16:21:44 +0200

    Add basegame error hook forwarding

--- lua/autorun/cfc_err_fwd.lua
+++ lua/autorun/cfc_err_fwd.lua
@@ -1,5 +1,11 @@
-ErrorForwarder = {}
+ErrorForwarder = ErrorForwarder or {}
+
+if SERVER then
+    AddCSLuaFile( "cfc_err_forwarder/branch.lua" )
+    AddCSLuaFile( "cfc_err_forwarder/client/forwarder.lua" )
+end
 
 if CLIENT then
     include( "cfc_err_forwarder/branch.lua" )
+    include( "cfc_err_forwarder/client/forwarder.lua" )
 end
